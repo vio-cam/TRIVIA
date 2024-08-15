b1b7ca0c1d8809731649b3728b3d4af0d645513e
viocamachomacedo-ctc <viocamachomacedo@crackthecode.la>
2024-08-14 20:11:42 -0500

    probando

--- js/script.js
+++ js/script.js
@@ -1,6 +1,31 @@
-function usuario() {
-    var nuevo = document.getElementById("usuario").value;
-    localStorage.setItem("name", nuevo);
-    window.location.href = '../TRIVIA/seccion.html';
+function next_user() {
+    var nombre = document.getElementById("user").value;
+    localStorage.setItem("name", nombre);
+    window.location.href = '../cole/conoc.html';
 }
+
+//nombre del usuario//
+
+function agregar_nombre() {
+    var anterior = document.getElementById("hola");
+    var actual = localStorage.getItem("name");
+    var mensaje = '<span> ahora elija una opcion para comenzar</span>';
+    actual = actual + mensaje; 
+    anterior.innerHTML = anterior.innerHTML + actual;
+
+}
+agregar_nombre(); 
+
+//conocimiento enlace//
+
+function next_pag1() {
+    window.location.href = '../cole/conocimiento_prev/previo.html';
+    
+}
+function next_pag2() {
+    window.location.href = '../cole/0cono.html';
+    
+}
+
+
 
